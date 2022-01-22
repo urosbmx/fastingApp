7d432bc8accdbcd9f034fe34ef949008bb43c282
urosbmx <urosbmx@gmail.com>
2022-01-22 19:45:00 +0100

    Initial Commit

---
+++ Fasting Timer/Fasting_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fasting_TimerApp.swift
+//  Fasting Timer
+//
+//  Created by Uroš Katanić on 22.1.22..
+//
+
+import SwiftUI
+
+@main
+struct Fasting_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
