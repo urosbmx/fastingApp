0c70becdb2f4d799ca96df97035c7631126ae6f3
urosbmx <urosbmx@gmail.com>
2022-01-27 23:31:08 +0100

    first commit

---
+++ Fasting Timer/FastingManager.swift
@@ -0,0 +1,8 @@
+//
+//  FastingManager.swift
+//  Fasting Timer
+//
+//  Created by Uroš Katanić on 27.1.22..
+//
+
+import Foundation
---
+++ Fasting Timer/ProgressRing.swift
@@ -0,0 +1,20 @@
+//
+//  ProgressRing.swift
+//  Fasting Timer
+//
+//  Created by Uroš Katanić on 22.1.22..
+//
+
+import SwiftUI
+
+struct ProgressRing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProgressRing_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressRing()
+    }
+}
